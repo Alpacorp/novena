7bbfe2e863323d7dc8eb2039c754cff6abee81ba
Alpacorp <alejandro.palacios88@gmail.com>
2021-12-14 23:08:47 -0500

    fix and add new styles

--- src/pages/Sings/Anton.js
+++ src/pages/Sings/Anton.js
@@ -1,12 +1,17 @@
 import * as React from 'react';
 import Back from '../../Components/Back';
+import './Sings.css';
 
 const Anton = () => {
   return (
     <>
-      <Back url='/stepseven' />
-      <h2>Anton Tiruliruliru</h2>
-      <iframe width="100%" height="315" src="https://www.youtube.com/embed/AJlU2aD4Rag" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <div className="steps">
+        <Back url='/stepseven' />
+        <h2>Anton Tiruliruliru</h2>
+        <div className='video'>
+          <iframe width="100%" height="315" src="https://www.youtube.com/embed/AJlU2aD4Rag" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+      </div>
     </>
   )
 };
